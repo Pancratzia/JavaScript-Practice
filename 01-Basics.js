a9bd1af417a985788854edfe62027092298dc432
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-21 00:29:29 -0400

    Renaming 01 file and adding variable lessons

---
+++ 01-Basics.js
@@ -0,0 +1,17 @@
+//My First Hello World!
+
+console.log("Hello Outside World!");
+
+(function () {
+  console.log("Hello Inside World!");
+})();
+
+//Learning about Variables
+
+var notUsed = "Var is not used anymore";
+let letVar = "Let is used";
+const constVar = "Const is used";
+
+console.log(`${notUsed}, ${letVar} & ${constVar}`);
+
+//BIG NOTE: Variables should start with lower case (Good Practice)
--- 01-HolaMundo.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello World outside IIFE");
-
-(function () {
-  console.log("Hello World inside IIFE. An IIFE is an anonymous function with its own scope and execution context");
-})();
