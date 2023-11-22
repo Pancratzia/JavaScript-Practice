5080756f19c6b939bab7c08d7af9c747730e49a1
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-22 01:26:29 -0400

    Resolving excercises

---
+++ 01-Excercises.js
@@ -0,0 +1,37 @@
+//Print from 1 to 10, 3 by 3
+
+for(let i = 1; i <= 10; i+=3){
+    console.log(i);
+}
+
+//Print from 1 to -10, 3 by 3
+
+for(let i = 1; i >= -10; i-=3){
+    console.log(i);
+}
+
+//Acummulator of addition from 1 to 5
+
+let sum = 0;
+for(let i = 1; i <= 5; i++){
+    sum += i;
+}
+console.log(sum);
+
+let i = 1;
+sum = 0;
+
+while(i <= 5){
+    sum += i;
+    i++;
+}
+console.log(sum);
+
+sum = 0;
+i = 1;
+
+do{
+    sum += i;
+    i++;
+}while(i <= 5);
+console.log(sum);
