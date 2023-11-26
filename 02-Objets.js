3221262f0d0e527574a7891ee378be1423720e27
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-26 11:17:04 -0400

    Imprimiendo las propiedades de cada objeto

--- 02-Objets.js
+++ 02-Objets.js
@@ -45,3 +45,23 @@ for(nombrePropiedad  in persona2){
 }
 
 persona.telefono = '123456';
+
+console.log(persona);
+
+delete persona.telefono;
+
+console.log(persona);
+
+
+//Imprimiendo objeto
+console.log(persona.nombre + ' ' + persona.apellido + ', ' + persona.edad);
+
+for (nombrePropiedad in persona){
+    console.log(persona[nombrePropiedad]);
+}
+
+let personaArray = Object.values(persona);
+console.log(personaArray);
+
+let personaString = JSON.stringify(persona);
+console.log(personaString);
