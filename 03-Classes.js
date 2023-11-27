7f857546aa337cafa50e48a802e4f6879373cdcf
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-26 20:06:47 -0400

    Metodos Get y Set en las clases

--- 03-Classes.js
+++ 03-Classes.js
@@ -2,11 +2,25 @@
 
 class Persona{
     constructor (nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this._nombre = nombre;
+        this._apellido = apellido;
     }
-    nombreCompleto(){
-        return this.nombre + ' ' + this.apellido;
+    //Metodos Get y Set
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get apellido(){
+        return this._apellido;
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
     }
 }
 
@@ -14,3 +28,6 @@ let persona1 = new Persona('Laura', 'Ortega');
 console.log(persona1);
 let persona2 = new Persona('Arthuro', 'Dugarte');
 console.log(persona2);
+
+persona1.nombre = 'Laura Estefanía';
+console.log(persona1.nombre);
