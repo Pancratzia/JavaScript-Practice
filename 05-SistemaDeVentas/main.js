c1cd2ecf4502eaebfd15a565b297939613d280d8
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-03 00:27:23 -0400

    Creando la clase Orden

--- 05-SistemaDeVentas/main.js
+++ 05-SistemaDeVentas/main.js
@@ -32,6 +32,50 @@ class Producto{
     }
 }
 
-let producto = new Producto("Tablet", 200);
+class Orden{
 
+    static contadorOrdenes = 0;
+
+    static get MAX_PRODUCTOS(){
+        return 5;
+    }
+
+    constructor(){
+        this._idOrden = ++Orden.contadorOrdenes;
+        this._fecha = new Date();
+        this._productos = [];
+        this._contadorProductosAgregados = 0;
+    }
+
+    get idOrden(){
+        return this._idOrden;
+    }
+
+    agregarProducto(producto){
+        if(this._productos.length <= Orden.MAX_PRODUCTOS){
+            this._productos.push(producto);
+            //this._productos[this._contadorProductosAgregados++] = producto; OTRA SINTAXIS
+        }else{
+            console.log("No se pueden agregar más productos");
+        }
+    }
+
+    calcularTotal(){
+        let total = 0;
+        for(let producto of this._productos){
+            total += producto.precio;
+        }
+        return total;
+    }
+
+    mostrarOrden(){
+        let productosOrden = '';
+        for(let producto of this._productos){
+            productosOrden += producto.toString() + '\n';
+        }
+        console.log(`Orden: ${this._idOrden}, fecha: ${this._fecha}, \n total: ${this.calcularTotal()}$, Productos: \n ${productosOrden}`);
+    }
+}
+
+let producto = new Producto("Tablet", 200);
 console.log(producto.toString());
