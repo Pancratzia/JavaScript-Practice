99753dcb6f1a2db79d1355129295c0fc1c31b6bb
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-04 00:14:45 -0400

    Creando la clase: Raton

--- 06-MundoPC/main.js
+++ 06-MundoPC/main.js
@@ -19,5 +19,24 @@ class DispositivoEntrada {
   set marca(marca) {
     this._marca = marca;
   }
+
+}
+
+class Raton extends DispositivoEntrada {
   
+    static contadorRatones = 0;
+
+    constructor(tipoEntrada, marca) {
+        super(tipoEntrada, marca);
+        this._idRaton = ++Raton.contadorRatones;
+    }
+
+    get idRaton() {
+        return this._idRaton;
+    }
+
+    toString() {
+        return `Raton: [idRaton: ${this._idRaton}, tipoEntrada: ${this._tipoEntrada}, marca: ${this._marca}]`;
+    }
+
 }
