4b6a3297b1c9541222656f6f1aa968cb01f2c019
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-05 14:56:00 -0400

    Reto 5 comenzando

---
+++ AdventJS/Reto#5-ElCyberTruckDeSanta.js
@@ -0,0 +1,34 @@
+/*
+Santa 🎅 está probando su nuevo trineo eléctrico, el CyberReindeer, en una carretera del Polo Norte. La carretera se representa con una cadena de caracteres, donde:
+
+. = Carretera
+S = Trineo de Santa
+* = Barrera abierta
+| = Barrera cerrada
+Ejemplo de carretera: S...|....|.....
+
+Cada unidad de tiempo, el trineo avanza una posición a la derecha. Si encuentra una barrera cerrada, se detiene hasta que la barrera se abra. Si está abierta, la atraviesa directamente.
+
+Todas las barreras empiezan cerradas, pero después de 5 unidades de tiempo, se abren todas para siempre.
+
+Crea una función que simule el movimiento del trineo durante un tiempo dado y devuelva un array de cadenas representando el estado de la carretera en cada unidad de tiempo:
+
+const road = 'S..|...|..'
+const time = 10 // unidades de tiempo
+const result = cyberReindeer(road, time)
+
+/* -> result:
+[
+  'S..|...|..', // estado inicial
+  '.S.|...|..', // avanza el trineo la carretera
+  '..S|...|..', // avanza el trineo la carretera
+  '..S|...|..', // el trineo para en la barrera
+  '..S|...|..', // el trineo para en la barrera
+  '...S...*..', // se abre la barrera, el trineo avanza
+  '...*S..*..', // avanza el trineo la carretera
+  '...*.S.*..', // avanza el trineo la carretera
+  '...*..S*..', // avanza el trineo la carretera
+  '...*...S..', // avanza por la barrera abierta
+]
+*/
+
