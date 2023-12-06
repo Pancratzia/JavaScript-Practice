8f0437dbd5c28d97b48d9dbea7f52a8e34c83cc5
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-06 00:14:19 -0400

    Sobreescritura de metodos y POO

---
+++ 07-POO/Sobreescritura.js
@@ -0,0 +1,40 @@
+class Empleado{
+    constructor(nombre, sueldo){
+        this._nombre = nombre;
+        this._sueldo = sueldo;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    get sueldo(){
+        return this._sueldo;
+    }
+
+    set sueldo(sueldo){
+        this._suelda = sueldo;
+    }
+
+    obtenerDetalles(){
+        return `Empleado: ${this._nombre} - Su sueldo es de ${this._sueldo}`;
+    }
+}
+
+class Gerente extends Empleado{
+    constructor(nombre, sueldo, departamento){
+        super (nombre, sueldo);
+        this._departamento = departamento;
+    }
+
+    obtenerDetalles(){
+        return `Gerente: ${this._nombre} - Su sueldo es de ${this._sueldo} - Departamento: ${this._departamento}`;
+    }
+
+}
+
+let empleado = new Empleado('Juan', 2000);
+console.log(empleado.obtenerDetalles());
+
+let gerente = new Gerente('Carlos', 3000, 'Sistemas');
+console.log(gerente.obtenerDetalles());
