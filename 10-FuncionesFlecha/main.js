e4ca4dde27a00326250770040b51ac4c8056fd3f
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-10 18:18:34 -0400

    Variaciones de las funciones flecha

--- 10-FuncionesFlecha/main.js
+++ 10-FuncionesFlecha/main.js
@@ -5,8 +5,36 @@ function miFuncion(){
     console.log("Esto es una función génerica");
 }
 
-const miFuncionFlecha = () => {
+let miFuncionFlecha = () => {
     console.log("Esto es una función flecha");
 }
 
+miFuncionFlecha = ()=> console.log("Esto es una función flecha"); //Función flecha de una sola línea
+
+
+const saludar = () => {
+    return "Hola";
+}
+
+const despedirse = () => "Adios";
+
+const regresarObjeto = () => ({
+    nombre: "Juan",
+    apellido: "Perez"
+});
+
+const funcionConParametros = msj => console.log(msj); 
+const funcionConDosParametros = (a, b) => a+b;
+const funcionConDosParametrosYMensaje = (a, b) => {
+    const suma = a+b;
+    return `${a} + ${b} = ${suma}`
+}
+
+
 miFuncionFlecha();
+console.log(saludar());
+console.log(despedirse());
+console.log(regresarObjeto().nombre + " " + regresarObjeto().apellido);
+funcionConParametros("Hola mundo");
+console.log(funcionConDosParametros(2, 3));
+console.log(funcionConDosParametrosYMensaje(2, 3));
