9033638407d8457c8e57d4c3cddc9a26352bc256
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-13 00:00:00 -0400

    Creando el html inicial para la practica de manipulacion del DOM

---
+++ 13-ManejandoElDOM/main.js
@@ -0,0 +1,8 @@
+//DOM (Document Object Model)
+
+/*
+
+Dentro del documento hay objetos como html, head, body, title, h1, footer...
+Esto es lo que se llama el modelo de objetos del DOM. Y es esto lo que se manipula con JS
+
+*/
