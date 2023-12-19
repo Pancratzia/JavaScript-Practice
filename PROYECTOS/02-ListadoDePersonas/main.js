1cf5589e412204c5abb9a6e75df6302745d6ad81
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-18 22:00:09 -0400

    Listado de personas finalizado

--- PROYECTOS/02-ListadoDePersonas/main.js
+++ PROYECTOS/02-ListadoDePersonas/main.js
@@ -1,14 +1,31 @@
-const personas = [ 
-    new Persona('Laura', 'Ortega'),
-    new Persona('Arthuro', 'Dugarte'),
+const personas = [
+  new Persona("Laura", "Ortega"),
+  new Persona("Arthuro", "Dugarte"),
 ];
 
+const forma = document.getElementById("forma");
 
+function mostrarPersonas() {
+  let html = "";
+  for (let persona of personas) {
+    html += `<li>${persona.nombre.toUpperCase()} ${persona.apellido.toUpperCase()}</li>`;
+  }
+  document.getElementById("personas").innerHTML = html;
+}
 
-function mostrarPersonas(){
-    let html = '';
-    for(let persona of personas){
-        html += `<li>${persona.nombre} ${persona.apellido}</li>`;
-    }
-    document.getElementById('personas').innerHTML = html;
+function limpiar() {
+  forma.reset();
+}
+
+function agregarPersona() {
+  const [nombre, apellido] = forma.elements;
+  if (!nombre.value || !apellido.value) {
+    alert("Llene todos los campos");
+    return;
+  }
+
+  const persona = new Persona(nombre.value, apellido.value);
+  personas.push(persona);
+  limpiar();
+  mostrarPersonas();
 }
