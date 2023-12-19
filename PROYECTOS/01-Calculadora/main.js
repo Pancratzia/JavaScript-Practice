823aaf8bd972426a8cbc82f312b8e90eea35815f
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-18 20:52:32 -0400

    Implementando la función de la suma

--- PROYECTOS/01-Calculadora/main.js
+++ PROYECTOS/01-Calculadora/main.js
@@ -1,3 +1,15 @@
+let numberA;
+let numberB;
+const divResultado = document.getElementById('resultado');
+
+function obtenerOperandos () {
+    const form = document.getElementById('form');
+    numberA = parseInt(form.numberA.value);
+    numberB = parseInt(form.numberB.value);
+}
+
 function sumar() {
-  alert("Suma");
+  obtenerOperandos();
+
+  divResultado.innerHTML = `${numberA} + ${numberB} = ${numberA + numberB}`;
 }
