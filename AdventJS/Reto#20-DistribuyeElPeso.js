dbc61068d036d9f63075220640905e3eb74f3b76
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-20 15:02:54 -0400

    Primera solución. 80 puntos

--- AdventJS/Reto#20-DistribuyeElPeso.js
+++ AdventJS/Reto#20-DistribuyeElPeso.js
@@ -67,5 +67,33 @@ Siempre son números enteros positivos.
 */
 
 function distributeGifts(weights) {
-  return weights;
+  const r = weights.length;
+  const c = weights[0].length;
+  let avgWeights = Array(r)
+    .fill()
+    .map(() => Array(c));
+  for (let i = 0; i < r; i++) {
+    for (let j = 0; j < c; j++) {
+      const gifts = [
+        weights[i - 1]?.[j],
+        weights[i + 1]?.[j],
+        weights[i]?.[j + -1],
+        weights[i]?.[j + 1],
+        weights[i]?.[j],
+      ].filter((n) => !!n);
+      let sum = 0;
+      gifts.map((n) => (sum += n));
+      avgWeights[i][j] = Math.round(sum / gifts.length);
+    }
+  }
+  return avgWeights;
 }
+
+
+const input = [
+  [4, 5, 1],
+  [6, null, 3],
+  [8, null, 4]
+]
+
+distributeGifts(input)
