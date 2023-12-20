429f12d6aac6f9c0027cc17ce6309b0f21849765
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-20 16:00:12 -0400

    mostrando ingreso, egreso y porcentaje con porcentajes

--- PROYECTOS/04-AplicacionDePresupuesto/js/main.js
+++ PROYECTOS/04-AplicacionDePresupuesto/js/main.js
@@ -5,19 +5,48 @@ const ingresos = [
 
 const egresos = [
     new Egreso('Alquiler', 900.00),
-    new Egreso('Comida', 300.00)
+    new Egreso('Comida', 300.00),
+    new Egreso('Deudas', 1720.00)
 ]
 
 const cargarApp = () => {
     cargarCabecero();
-    cargarIngresos();
-    cargarEgresos();
 }
+
+const totalizar = (array) => {
+    let total = 0;
+    for (let element of array) {
+        total += element.valor;
+    }
+
+    return total;
+}   
+
 
 const cargarCabecero = () => {
-    
+    const ingresosTotales = totalizar(ingresos);
+    const egresosTotales = totalizar(egresos);
+
+    const presupuesto = ingresosTotales - egresosTotales;
+    const porcentajeEgreso = (egresosTotales / ingresosTotales);
+
+    document.getElementById('presupuesto').innerHTML = formatoMoneda(presupuesto);;
+    document.getElementById('porcentaje').innerHTML = formatoPorcentaje(porcentajeEgreso);
+    document.getElementById('ingresos').innerHTML = formatoMoneda(ingresosTotales);
+    document.getElementById('egresos').innerHTML = formatoMoneda(egresosTotales);
 }
 
-const cargarIngresos = () => {}
+const formatoMoneda = (valor) => {
+    return valor.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    });
+}
 
-const cargarEgresos = () => {}
+const formatoPorcentaje = (valor) => {
+    return valor.toLocaleString('en-US', {
+        style: 'percent',
+        minimumFractionDigits: 2
+    });
+}
