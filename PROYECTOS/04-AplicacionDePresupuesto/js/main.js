628acfc3fca9566048610c2311cb3668ff865630
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-20 15:50:56 -0400

    Comenzando con el código del main.js

---
+++ PROYECTOS/04-AplicacionDePresupuesto/js/main.js
@@ -0,0 +1,23 @@
+const ingresos = [
+    new Ingreso('Salario', 2100.00),
+    new Ingreso('Venta de un auto', 15000.00)
+];
+
+const egresos = [
+    new Egreso('Alquiler', 900.00),
+    new Egreso('Comida', 300.00)
+]
+
+const cargarApp = () => {
+    cargarCabecero();
+    cargarIngresos();
+    cargarEgresos();
+}
+
+const cargarCabecero = () => {
+    
+}
+
+const cargarIngresos = () => {}
+
+const cargarEgresos = () => {}
