814fe6303946593238dc145c1706e088b3f0ed2f
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-21 15:17:47 -0400

    Aprendiendo sobre decoradores

---
+++ 15-TypeScript/07-Decoradores/decoradores.js
@@ -0,0 +1,14 @@
+"use strict";
+/*function Saludar(target: Function): void{
+    target.prototype.saludo = () => {
+        alert("Hola! Desde los decoradores");
+    }
+}
+
+@Saludar
+class Hola{
+    constructor() {}
+}
+
+let hola = new Hola();
+hola.saludo();*/ 
