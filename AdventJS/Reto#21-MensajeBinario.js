d82638d89801da99a46b0c01a181a892d57df096
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-21 18:53:47 -0400

    Primera solución. 180 puntos

--- AdventJS/Reto#21-MensajeBinario.js
+++ AdventJS/Reto#21-MensajeBinario.js
@@ -21,5 +21,23 @@ Dicen que si encuentran el patrón, podrán enviar un mensaje de vuelta a Marte 
 */
 
 function findBalancedSegment(message) {
-  return [];
+
+  let maxSegment = [];
+
+  for (let i = 0; i < message.length; i++) {
+    let zeros = 0;
+    let ones = 0;
+    for (let j = i; j < message.length; j++) {
+      if (message[j] === 0) {
+        zeros++;
+      } else {
+        ones++;
+      }
+      if (zeros === ones && j - i > (maxSegment[1] - maxSegment[0] || 0)) {
+        maxSegment = [i, j];
+      }
+    }
+  }
+
+  return maxSegment.length > 0 ? maxSegment : [];
 }
