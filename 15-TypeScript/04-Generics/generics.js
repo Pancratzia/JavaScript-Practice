7ccf22f0c18a3c4a7465dd0464fb7a0702e68602
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-21 14:47:37 -0400

    Aprendiendo sobre generics

---
+++ 15-TypeScript/04-Generics/generics.js
@@ -0,0 +1,6 @@
+"use strict";
+const arrayOfNumbers = [1, 1, 2, 3, 5];
+arrayOfNumbers.forEach((number) => {
+    document.write(`${number}<br>`);
+});
+// arrayOfNumbers.push("Laura"); - Error
---
+++ 15-TypeScript/04-Generics/generics.ts
@@ -0,0 +1,7 @@
+const arrayOfNumbers: Array<number> = [1, 1, 2, 3, 5];
+
+arrayOfNumbers.forEach((number) => {
+    document.write(`${number}<br>`);
+})
+
+// arrayOfNumbers.push("Laura"); - Error
