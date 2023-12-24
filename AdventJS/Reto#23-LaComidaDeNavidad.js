761529063e5a1a40a4c4dda6109182774081e8ce
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-24 16:02:21 -0400

    Primera solución al tero #23. 160 puntos

--- AdventJS/Reto#23-LaComidaDeNavidad.js
+++ AdventJS/Reto#23-LaComidaDeNavidad.js
@@ -42,5 +42,18 @@ Si no hay ingredientes repetidos, devolvemos un array vacío.
 */
 
 function organizeChristmasDinner(dishes) {
-  return [];
+  const ingredients = {}
+
+  for (let dish of dishes)
+    for (let ingredient of dish.slice(1))
+      ingredients[ingredient] ?
+        ingredients[ingredient].push(dish[0]) :
+        ingredients[ingredient] = [dish[0]]
+
+  let output = []
+  for (let key of Object.keys(ingredients).sort())
+    if (ingredients[key].length > 1)
+      output.push([key, ...ingredients[key].sort()])
+
+  return output
 }
