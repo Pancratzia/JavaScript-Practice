5a5170fd847ea18395e9d366acca866e4990ecb4
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-26 16:54:59 -0400

    Ejercicio del dia 25 resuelto. 160 puntos

--- AdventJS/Reto#25-CalculandoDistancias.js
+++ AdventJS/Reto#25-CalculandoDistancias.js
@@ -37,5 +37,17 @@ Los números de los niños nunca se repiten.
 */
 
 function travelDistance(map) {
-  return 0;
+  const pos = {};
+  let distance = 0;
+  for (const [x, line] of map.split("\n").entries()) {
+    for (let i of ["S", 1, 2, 3, 4, 5, 6, 7, 8, 9]) {
+      line.includes(i) ? (pos[i] = [x, line.indexOf(i)]) : null;
+    }
+  }
+  pos[0] = pos["S"];
+  for (let i = 0; i < Object.keys(pos).length - 2; i++) {
+    distance +=
+      Math.abs(pos[i][0] - pos[i + 1][0]) + Math.abs(pos[i][1] - pos[i + 1][1]);
+  }
+  return distance;
 }
